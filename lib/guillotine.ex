f355cee53a7c1a9fb7f5440ea191c1d24d209fb5
Your Name <you@example.com>
2018-06-12 04:25:58 -0400

    First commit

---
+++ lib/guillotine.ex
@@ -0,0 +1,18 @@
+defmodule Guillotine do
+  @moduledoc """
+  Documentation for Guillotine.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Guillotine.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/guillotine_test.exs
@@ -0,0 +1,8 @@
+defmodule GuillotineTest do
+  use ExUnit.Case
+  doctest Guillotine
+
+  test "greets the world" do
+    assert Guillotine.hello() == :world
+  end
+end
