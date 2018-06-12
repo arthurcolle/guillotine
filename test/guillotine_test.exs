343ee6a9f53e8ea171be3dae365e66fbf74bcc8c
Your Name <you@example.com>
2018-06-12 05:56:54 -0400

    Ok done

--- test/guillotine_test.exs
+++ test/guillotine_test.exs
@@ -5,6 +5,6 @@ defmodule GuillotineTest do
   test "greets the world" do
   	str1 = "ABCD" # 2
   	str2 = "1234" # 1
-    assert Guillotine.exec(str1, str2) := "AB1CD23"
+    assert Guillotine.exec(str1, str2) == 'AB1CD23'
   end
 end
