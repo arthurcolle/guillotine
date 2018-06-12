0497233e41ad8efa8fd35ea46ceed7641b48ff45
Your Name <you@example.com>
2018-06-12 05:29:24 -0400

    Added logic, removed manipulator

--- lib/guillotine.ex
+++ lib/guillotine.ex
@@ -1,18 +1,29 @@
 defmodule Guillotine do
-  @moduledoc """
-  Documentation for Guillotine.
-  """
+  def exec(str1, str2) do # str1 = "ABCDEF" and str2 = "12345"
+    c1 = String.to_charlist(str1)
+    c2 = String.to_charlist(str2)
+    chunked_1 = Enum.chunk_every(c1, 2, 2)
+    chunked_2 = Enum.chunk_every(c2, 1, 1)
+    Guillotine.guillotine(chunked_1, chunked_2)
+  end
 
-  @doc """
-  Hello world.
+  def guillotine([], []) do
+    ""
+  end
 
-  ## Examples
+  def guillotine([], a) do
+    hd(a)
+  end
 
-      iex> Guillotine.hello
-      :world
+  def guillotine(a,b) do
+    hd(a)++hd(b)++guillotine(tl(a),tl(b))
+  end
 
-  """
-  def hello do
-    :world
+  def run() do
+    str1 = "WXYZ"
+    str2 = "123"
+    Guillotine.exec(str1, str2) # WX1YZ2
   end
 end
+
+IO.puts Guillotine.run()
--- test/guillotine_test.exs
+++ test/guillotine_test.exs
@@ -3,6 +3,8 @@ defmodule GuillotineTest do
   doctest Guillotine
 
   test "greets the world" do
-    assert Guillotine.hello() == :world
+  	str1 = "ABCD" # 2
+  	str2 = "1234" # 1
+    assert Guillotine.exec(str1, str2) := "AB1CD23"
   end
 end
